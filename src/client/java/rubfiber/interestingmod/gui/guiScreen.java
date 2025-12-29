054c11c2479e60805b1b6970bb06e1b711a74016
rubfiber <abhirkarnik@gmail.com>
2025-12-29 17:29:35 -0500

    Fixed color adjuster

--- src/client/java/rubfiber/interestingmod/gui/guiScreen.java
+++ src/client/java/rubfiber/interestingmod/gui/guiScreen.java
@@ -8,10 +8,11 @@ import net.minecraft.client.gui.components.EditBox;
 import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.client.input.CharacterEvent;
 import net.minecraft.client.input.KeyEvent;
-import net.minecraft.client.input.MouseButtonEvent;
 import net.minecraft.network.chat.Component;
 import org.jspecify.annotations.NonNull;
 import org.lwjgl.glfw.GLFW;
+
+import java.awt.*;
 
 public class guiScreen extends Screen {
 
@@ -19,11 +20,20 @@ public class guiScreen extends Screen {
         super(title);
     }
     EditBox nameBox;
-
-
+    EditBox redBox;
+    EditBox greenBox;
+    EditBox blueBox;
+    ColorChangerWidget colorChangerWidget;
 
     public boolean charTyped(@NonNull CharacterEvent event) {
-        return nameBox.isVisible() && nameBox.charTyped(event);
+        if (nameBox.isFocused() && nameBox.charTyped(event)) return true;
+
+
+        if (redBox.isFocused() && redBox.charTyped(event)) return true;
+        if (greenBox.isFocused() && greenBox.charTyped(event)) return true;
+        if (blueBox.isFocused() && blueBox.charTyped(event)) return true;
+
+        return super.charTyped(event);
     }
 
     public boolean keyPressed(KeyEvent event) {
@@ -31,26 +41,16 @@ public class guiScreen extends Screen {
             this.onClose();
             return true;
         }
-     //   if (event.key() == GLFW.GLFW_KEY_ENTER) {
-       //     nameBox.setFocused(false);
-       // }
+        if (nameBox.isFocused() && nameBox.keyPressed(event)) return true;
 
-        return nameBox.isVisible() && nameBox.keyPressed(event);
+
+        if (redBox.isFocused() && redBox.keyPressed(event)) return true;
+        if (greenBox.isFocused() && greenBox.keyPressed(event)) return true;
+        if (blueBox.isFocused() && blueBox.keyPressed(event)) return true;
+
+        return super.keyPressed(event);
     }
-    public boolean mouseClicked(MouseButtonEvent event) {
-        // Let the textbox try first
-        if (nameBox.isVisible() && nameBox.mouseClicked(event, true)) {
-            return true; // EditBox focused itself
-        }
 
-        // Clicked outside → unfocus
-        if (nameBox.isFocused()) {
-            nameBox.setFocused(false);
-        }
-
-        // Let Screen handle buttons, etc.
-        return super.mouseClicked(event, true);
-    }
 
     @Override
     protected void init() {
@@ -63,20 +63,39 @@ public class guiScreen extends Screen {
                 15,
                 Component.literal("Input")
         );
+        redBox = new EditBox(font, 200, 125, 40, 15, Component.literal("Input"));
+        redBox.setMaxLength(3);
+        redBox.setBordered(true);
+        redBox.setVisible(false);
+        redBox.setFocused(false);
+        greenBox = new EditBox(font, 260, 125, 40, 15, Component.literal("Input"));
+        greenBox.setMaxLength(3);
+        greenBox.setBordered(true);
+        greenBox.setVisible(false);
+        greenBox.setFocused(false);
+        blueBox = new EditBox(font, 320, 125, 40, 15, Component.literal("Input"));
+        blueBox.setMaxLength(3);
+        blueBox.setBordered(true);
+        blueBox.setVisible(false);
+        blueBox.setFocused(false);
+
         nameBox.setMaxLength(64);
         nameBox.setBordered(true);
         nameBox.setVisible(false);
         nameBox.setFocused(false);
         nameBox.setValue("My Waypoint");
 
-        ColorChangerWidget colorChangerWidget = new ColorChangerWidget(300, 300, 100, 100, font);
+        colorChangerWidget = new ColorChangerWidget(300, 300, 100, 100, font);
         colorChangerWidget.setVisible(false);
         this.addRenderableWidget(colorChangerWidget);
         Button buttonWidget = Button.builder(Component.nullToEmpty("Create waypoint"), (btn) -> {
             nameBox.setVisible(true);
-            nameBox.setFocused(true);
+            redBox.setVisible(true);
+            greenBox.setVisible(true);
+            blueBox.setVisible(true);
             colorChangerWidget.setVisible(true);
             String name = nameBox.getValue();
+
 
 
             // When the button is clicked, we can display a toast to the screen.
@@ -92,6 +111,9 @@ public class guiScreen extends Screen {
         // Register the button widget.
         this.addRenderableWidget(buttonWidget);
         this.addRenderableWidget(nameBox);
+        this.addRenderableWidget(redBox);
+        this.addRenderableWidget(greenBox);
+        this.addRenderableWidget(blueBox);
 
     }
 
@@ -104,7 +126,16 @@ public class guiScreen extends Screen {
         // We'll subtract the font height from the Y position to make the text appear above the button.
         // Subtracting an extra 10 pixels will give the text some padding.
         // textRenderer, text, x, y, color, hasShadow
-        context.drawString(this.font, "Waypoint builder", 40, 40 - this.font.lineHeight - 10, 0xFFFFFFFF, true);
+        context.drawString(this.font, "Waypoint Workshop", 40, 40 - this.font.lineHeight - 10, 0xFFFFFFFF, true);
+
+        if (emptyStringToZero(redBox.getValue()) > 255 && !redBox.isFocused()) redBox.setValue("255");
+        if (emptyStringToZero(greenBox.getValue()) > 255 && !greenBox.isFocused()) greenBox.setValue("255");
+        if (emptyStringToZero(blueBox.getValue()) > 255 && !blueBox.isFocused()) blueBox.setValue("255");
+
+        colorChangerWidget.changeColor(emptyStringToZero(redBox.getValue()), emptyStringToZero(greenBox.getValue()), emptyStringToZero(blueBox.getValue()));
+    }
+    public static int emptyStringToZero(String s) {
+        return (s.isBlank()) ? 0 : Integer.parseInt(s);
     }
 }
 
